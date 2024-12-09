c63e72ddb5a66ec5953759b942ff453c9c093fe4
Carlos Buruel <charly.ob.26@gmail.com>
2024-12-09 12:59:45 -0600

    379. Manejo de errores

--- lib/features/auth/infrastructure/datasources/auth_datasource_impl.dart
+++ lib/features/auth/infrastructure/datasources/auth_datasource_impl.dart
@@ -27,8 +27,12 @@ class AuthDataSourceImpl extends AuthDatasource {
 
       final user = UserMapper.userJsonToEntity(response.data);
       return user;
+    } on DioException catch (e) {
+      if (e.response?.statusCode == 401) throw WrongCredentials();
+      if (e.type == DioExceptionType.connectionTimeout) throw ConnectionTimeout();
+      throw CustomError('Something wrong happend', 1);
     } catch (e) {
-      throw WrongCredentials();
+      throw CustomError('Something wrong happend', 1);
     }
   }
 
