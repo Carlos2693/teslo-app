5ee7c15954db7a7a016a3519529c70c0adac1140
Carlos Buruel <charly.ob.26@gmail.com>
2024-12-15 17:30:13 -0600

    387. Implementar el patrón adaptador

--- lib/features/auth/presentation/providers/auth_provider.dart
+++ lib/features/auth/presentation/providers/auth_provider.dart
@@ -45,6 +45,7 @@ class AuthNotifier extends StateNotifier<AuthState> {
     state = state.copyWith(
       user: user,
       authStatus: AuthStatus.authenticated,
+      errorMessage: '',
     );
   }
 
