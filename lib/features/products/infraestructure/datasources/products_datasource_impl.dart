04737954b8540bc224b5f675d4a3cc0728e04254
Carlos Buruel <charly.ob.26@gmail.com>
2025-01-05 14:17:18 -0600

    402. Implementación - getProductsByPage

--- lib/features/products/infraestructure/datasources/products_datasource_impl.dart
+++ lib/features/products/infraestructure/datasources/products_datasource_impl.dart
@@ -1,6 +1,24 @@
+import 'package:dio/dio.dart';
+import 'package:teslo_shop/config/constants/environment.dart';
+
 import 'package:teslo_shop/features/products/domain/domain.dart';
 
 class ProductsDatasourceImpl extends ProductsDatasource {
+
+  late final Dio dio;
+  final String accessToken;
+
+  ProductsDatasourceImpl({
+    required this.accessToken,
+  }) : dio = Dio(
+    BaseOptions(
+      baseUrl: Environment.apiUrl,
+      headers: {
+        'Authorization': 'Bearer $accessToken'
+      }
+    )
+  );
+
   @override
   Future<Product> createUpdateProduct(Map<String, dynamic> productLike) {
     // TODO: implement createUpdateProduct
@@ -14,9 +32,13 @@ class ProductsDatasourceImpl extends ProductsDatasource {
   }
 
   @override
-  Future<List<Product>> getProductsByPage({int limit = 10, int offset = 0}) {
-    // TODO: implement getProductsByPage
-    throw UnimplementedError();
+  Future<List<Product>> getProductsByPage({int limit = 10, int offset = 0}) async {
+    final response = await dio.get<List>('/api/products?limit=$limit&offset=$offset');
+    final List<Product> products = [];
+    for (var product in response.data ?? []) {
+      // products.add(null);
+    }
+    return products;
   }
 
   @override
